baa74e4ece9b22a99ca302f3ac8cf0dd47f0c307
Michał Walenciak <michalwalenciak@gmail.com>
2025-04-12 11:12:35 +0200

    Add tool for scoped dir

--- twotone/tools/utils2/files.py
+++ twotone/tools/utils2/files.py
@@ -1,4 +1,5 @@
 
+import shutil
 from pathlib import Path
 
 
@@ -6,3 +7,17 @@ def split_path(path: str) -> (str, str, str):
     info = Path(path)
 
     return str(info.parent), info.stem, info.suffix[1:]
+
+
+class ScopedDirectory:
+    def __init__(self, path: Path):
+        self.path = Path(path)
+
+    def __enter__(self):
+        if self.path.exists():
+            shutil.rmtree(self.path)
+        self.path.mkdir(parents=True, exist_ok=False)
+        return self.path  # optional: return the usable Path object
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self.path, ignore_errors=True)
