f89710bf1257c737ea8acdc0a96cb75d274b308e
Michał Walenciak <michalwalenciak@gmail.com>
2025-04-29 19:41:46 +0200

    Extract JellyFin class to a separate file

---
+++ twotone/tools/melt/duplicates_source.py
@@ -0,0 +1,12 @@
+
+from typing import Dict, List
+
+from .. import utils
+
+
+class DuplicatesSource:
+    def __init__(self, interruption: utils.InterruptibleProcess):
+        self.interruption = interruption
+
+    def collect_duplicates(self) -> Dict[str, List[str]]:
+        pass
--- twotone/tools/melt/melt.py
+++ twotone/tools/melt/melt.py
@@ -7,7 +7,6 @@ import logging
 import numpy as np
 import os
 import re
-import requests
 
 from collections import defaultdict
 from overrides import override
@@ -19,106 +18,16 @@ from typing import Callable, Dict, List, Tuple
 from .. import utils
 from ..tool import Tool
 from ..utils2 import files, image, process, video
+from .duplicates_source import DuplicatesSource
 
 
 type FramesInfo = Dict[int, Dict[str, str]]
-
-
-class DuplicatesSource:
-    def __init__(self, interruption: utils.InterruptibleProcess):
-        self.interruption = interruption
-
-    def collect_duplicates(self) -> Dict[str, List[str]]:
-        pass
-
 
 def _split_path_fix(value: str) -> List[str]:
     pattern = r'"((?:[^"\\]|\\.)*?)"'
 
     matches = re.findall(pattern, value)
     return [match.replace(r'\"', '"') for match in matches]
-
-
-class JellyfinSource(DuplicatesSource):
-    def __init__(self, interruption: utils.InterruptibleProcess, url: str, token: str, path_fix: Tuple[str, str]):
-        super().__init__(interruption)
-
-        self.url = url
-        self.token = token
-        self.path_fix = path_fix
-
-    def _fix_path(self, path: str) -> str:
-        fixed_path = path
-        if self.path_fix:
-            pfrom = self.path_fix[0]
-            pto = self.path_fix[1]
-
-            if path.startswith(pfrom):
-                fixed_path = f"{pto}{path[len(pfrom):]}"
-            else:
-                logging.error(f"Could not replace \"{pfrom}\" in \"{path}\"")
-
-        return fixed_path
-
-
-    def collect_duplicates(self) -> Dict[str, List[str]]:
-        endpoint = f"{self.url}"
-        headers = {
-            "X-Emby-Token": self.token
-        }
-
-        paths_by_id = defaultdict(lambda: defaultdict(list))
-
-        def fetchItems(params: Dict[str, str] = {}):
-            self.interruption._check_for_stop()
-            params.update({"fields": "Path,ProviderIds"})
-
-            response = requests.get(endpoint + "/Items", headers=headers, params=params)
-            if response.status_code != 200:
-                raise RuntimeError("No access")
-
-            responseJson = response.json()
-            items = responseJson["Items"]
-
-            for item in items:
-                name = item["Name"]
-                id = item["Id"]
-                type = item["Type"]
-
-                if type == "Folder":
-                    fetchItems(params={"parentId": id})
-                elif type == "Movie":
-                    providers = item["ProviderIds"]
-                    path = item["Path"]
-
-                    for provider, id in providers.items():
-                        # ignore collection ID
-                        if provider != "TmdbCollection":
-                            paths_by_id[provider][id].append((name, path))
-
-        fetchItems()
-        duplicates = {}
-
-        for provider, ids in paths_by_id.items():
-            for id, data in ids.items():
-                if len(data) > 1:
-                    names, paths = zip(*data)
-
-                    fixed_paths = [self._fix_path(path) for path in paths]
-
-                    # all names should be the same
-                    same = all(x == names[0] for x in names)
-
-                    if same:
-                        name = names[0]
-                        duplicates[name] = fixed_paths
-                    else:
-                        names_str = '\n'.join(names)
-                        paths_str = '\n'.join(fixed_paths)
-                        logging.warning(f"Different names for the same movie ({provider}: {id}):\n{names_str}.\nJellyfin files:\n{paths_str}\nSkipping.")
-
-        return duplicates
-
 
 class PhashCache:
     def __init__(self, hash_size: int = 16):
