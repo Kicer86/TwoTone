30c86610c3e3c64ae0f6d3701c369ea172a5a6a5
Michał Walenciak <michalwalenciak@gmail.com>
2025-07-12 09:37:28 +0200

    Verify get_video_data()'s results

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,10 @@
 
 import os
 import unittest
+from parameterized import parameterized
 
-from twotone.tools.utils import subtitles_utils
-from common import WorkingDirectoryForTest, TwoToneTestCase, write_subtitle
+from twotone.tools.utils import subtitles_utils, video_utils
+from common import WorkingDirectoryForTest, TwoToneTestCase, get_video, write_subtitle
 
 
 class UtilsTests(TwoToneTestCase):
@@ -26,6 +27,70 @@ class UtilsTests(TwoToneTestCase):
         self._test_content("12:test", False)
         self._test_content("{12}:test", False)
         self._test_content("{a}{b}:test", False)
+
+
+    test_videos = [
+        # case: merge all audio tracks
+        (
+            "MP4 - camera",
+            # input
+            "DSC_8073.MP4",
+            # expected output
+            {
+                'video':
+                [{
+                    'fps': '30000/1001',
+                    'length': 3403,
+                    'width': 3840,
+                    'height': 2160,
+                    'bitrate': None,
+                    'codec': 'hevc',
+                    'attached_pic': False,
+                    'tid': 0
+                }],
+                'audio':
+                [{
+                    'language': 'eng',
+                    'channels': 2,
+                    'sample_rate': '48000',
+                    'tid': 1
+                }]
+            }
+        ),
+        (
+            "MP4 - camera2",
+            # input
+            "moon.mp4",
+            # expected output
+            {
+                'video':
+                [{
+                    'fps': '29999/500',
+                    'length': 1000,
+                    'width': 2160,
+                    'height': 3840,
+                    'bitrate': None,
+                    'codec': 'hevc',
+                    'attached_pic': False,
+                    'tid': 0
+                }],
+                'audio':
+                [{
+                    'language': 'eng',
+                    'channels': 2,
+                    'sample_rate': '48000',
+                    'tid': 1
+                }]
+            }
+        ),
+    ]
+
+    @parameterized.expand(test_videos)
+    def test_video_info(self, name, input, expected_streams):
+        input_file_name = get_video(input)
+        file_info = video_utils.get_video_data(input_file_name)
+
+        self.assertEqual(file_info, expected_streams)
 
 
 if __name__ == '__main__':
