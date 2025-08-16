9db4219e035fadd93e4d42b3485021c033571728
Michał Walenciak <Kicer86@gmail.com>
2025-08-16 16:53:47 +0200

    chore: quiet mypy regression

--- twotone/tools/utils/process_utils.py
+++ twotone/tools/utils/process_utils.py
@@ -7,7 +7,7 @@ import subprocess
 from dataclasses import dataclass
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
-from typing import List, Dict
+from typing import Any, Dict, List
 
 from . import generic_utils
 from . import video_utils
@@ -37,7 +37,7 @@ def start_process(process: str, args: List[str], show_progress = False) -> Proce
 
     full_cmd = f"{process} {' '.join(args)}"
     logging.debug(f"Starting {full_cmd}")
-    popen_kwargs = {
+    popen_kwargs: Dict[str, Any] = {
         "stdout": subprocess.PIPE,
         "stderr": subprocess.PIPE,
         "universal_newlines": True,
@@ -46,7 +46,7 @@ def start_process(process: str, args: List[str], show_progress = False) -> Proce
     if os.name != "nt":
         popen_kwargs["preexec_fn"] = os.setsid
     else:
-        popen_kwargs["creationflags"] = subprocess.CREATE_NEW_PROCESS_GROUP
+        popen_kwargs["creationflags"] = getattr(subprocess, "CREATE_NEW_PROCESS_GROUP", 0)
     sub_process = subprocess.Popen(command, **popen_kwargs)
 
     if show_progress:
