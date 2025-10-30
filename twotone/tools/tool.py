c63029d5f76704a2adb0b0a0a0f3aded9601f6ab
Michał Walenciak <michalwalenciak@gmail.com>
2025-10-30 17:41:11 +0100

    Restore formatting

--- twotone/tools/tool.py
+++ twotone/tools/tool.py
@@ -6,10 +6,8 @@ class Tool:
     def setup_parser(self, parser: argparse.ArgumentParser):
         pass
 
-    def analyze(self, args: argparse.Namespace, logger: logging.Logger,
-                working_dir: str):
+    def analyze(self, args: argparse.Namespace, logger: logging.Logger, working_dir: str):
         pass
 
-    def perform(self, args: argparse.Namespace,
-                logger: logging.Logger, working_dir: str):
+    def perform(self, args: argparse.Namespace, logger: logging.Logger, working_dir: str):
         pass
