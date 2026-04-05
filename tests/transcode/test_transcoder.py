9c2baacf0614637ca76ee399ddfcc705fea9a208
Michał Walenciak <michalwalenciak@gmail.com>
2026-04-05 21:09:44 +0200

    Remove slowest transcode integration test (segments vs no-segments)

--- tests/transcode/test_transcoder.py
+++ tests/transcode/test_transcoder.py
@@ -13,16 +13,6 @@ class TranscoderTests(TwoToneTestCase):
 
         self.assertEqual(best_enc, 28)
 
-    def test_video_with_segments_and_no_segments(self):
-        transcoder = Transcoder(self.wd.path, self.logger)
-        for test_video, crf in [(get_video("10189155-hd_1920_1080_25fps.mp4"), 27),
-                                (get_video("big_buck_bunny_720p_10mb.mp4"), 29)]:
-            best_enc_segments = transcoder.find_optimal_crf(test_video, allow_segments=True)
-            best_enc_no_segments = transcoder.find_optimal_crf(test_video, allow_segments=False)
-
-            self.assertEqual(best_enc_no_segments, crf)
-            self.assertTrue(abs(best_enc_no_segments - best_enc_segments) < 2)
-
     def test_transcoding_on_short_videos_dry_run(self):
         add_test_media("VID_20240412_1815.*", self.wd.path, copy = True)
         hashes_before = hashes(self.wd.path)
